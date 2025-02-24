0d6c6ee1c66650789928f901ee43232cec3143cd
Sapar-s <smagulsapar52@gmail.com>
2025-02-24 12:29:06 +0800

    first mongoDB test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { getMovies } from "./data-base/mongodb";
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +9,16 @@ const port = 5000;
 
 app.get("/", (req, res) => {
   res.send("helloo woorld");
+});
+
+app.get("/movies", async (req, res) => {
+  try {
+    const { year, imdb } = await req.query;
+    const movies = await getMovies(Number(year), Number(imdb));
+    res.status(201).json({ message: "success", movies: movies });
+  } catch (error) {
+    res.status(500).json({ message: "error", error });
+  }
 });
 
 app.listen(port, () => {
